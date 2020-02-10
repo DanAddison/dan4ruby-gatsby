e3b94389c5d258284efa86e26257b168aa6fa142
Dan Addison <danaddisoncreative@gmail.com>
2020-02-10 16:58:50 +0000

    Tidying up Sidebar by trying out Styled System props

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,74 +1,49 @@
 import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
-import { color, layout } from 'styled-system'
+import { color, layout, space, typography, flexbox } from 'styled-system'
 
-const HeaderOuter = styled.header`
+const Wrapper = styled.header`
   position: absolute;
   top: 0;
   bottom: 0;
   z-index: 100;
-  color: ${props => props.theme.white};
   ${layout}
-
-  @media (min-width: ${props => props.theme.sidenav}) {
-    left: 0;
-  }
-`
-
-const HeaderInner = styled.div`
-  flex-direction: column;
-  margin: 0 auto;
-  max-width: 60em;
-  padding: 2rem;
-  display: flex;
-  height: 100%;
   ${color}
+  ${space}
+  ${flexbox}
 `
 
-const MainNav = styled.nav`
-  margin-left: auto;
-  text-align: right;
-  margin-right: -1rem;
-  font-size: 1.25rem;
-
-  @media (min-width: ${props => props.theme.sidenav}) {
-    margin-left: 0;
-    text-align: left;
-    margin-top: 2em;
-  }
+const Navigation = styled.nav`
+  ${color}
+  ${space}
+  ${typography}
 `
 
 const Credits = styled.div`
-  display: none;
-
-  @media (min-width: ${props => props.theme.sidenav}) {
-    display: block;
-    margin-top: auto;
-
-    p {
-      margin-bottom: 1em;
-    }
-  }
+  margin-top: auto;
+  ${color}
+  ${typography}
 `
 
 const Sidebar = ({menuLinks}) => (
-  <HeaderOuter width={['200px','500px']}>
-    <HeaderInner bg={'primary'}>
-      <MainNav>
-        <ul>
-          {menuLinks.map(link => (
-            <li key={link.name}>
-              <Link to={link.link}>{link.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </MainNav>
-      <Credits>
-        <p>© {new Date().getFullYear()} Ruby & <a href="https://danaddisoncreative.com">Dan Addison</a></p>
-      </Credits>
-    </HeaderInner>
-  </HeaderOuter>
+  <Wrapper display={'flex'} flexDirection='column' width={['150px','200px']} bg={'primary'} p={4}>
+
+    <Navigation color={'white'} mt={4} fontSize={[2,2,3]}>
+      <ul>
+        {menuLinks.map(link => (
+          <li key={link.name}>
+            <Link to={link.link}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </Navigation>
+
+    <Credits color={'white'}>
+      <p>© {new Date().getFullYear()} Ruby & <a href="https://danaddisoncreative.com">Dan Addison</a></p>
+    </Credits>
+
+  </Wrapper>
 )
 
 
