ff93818e6833ceea1f8187f9ffd99f15fb4bd867
Dan Addison <danaddisoncreative@gmail.com>
2020-02-24 16:22:23 +0000

    Prevent gallery images overflowing on mobiles

--- src/components/gallery/Gallery.styled.js
+++ src/components/gallery/Gallery.styled.js
@@ -6,12 +6,22 @@ export const GalleryWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   /* now override flexbox fallback with grid for supporting browsers */
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-  grid-gap: 1rem;
+  /* don't add grid until over 400px wide due to the min width of each cell being 400px */
+  @media (min-width: 420px) {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+    grid-gap: 1rem;
+  }
 `
 
 export const GalleryImage = styled.div`
   width: 100%;
   max-width: 600px;
+  /* until display grid kicks in */
+  padding-bottom: 1em;
+
+    @media (min-width: 420px) {
+      padding-bottom: 0;
+    }
+
 `;
